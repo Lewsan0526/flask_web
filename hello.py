2080f094fb04d4c60f1b63be924c62def2b9b955
lewsan <liujintong@peiwo.cn>
2018-02-07 21:35:25 +0800

    feat: import bootstrap to templates

--- hello.py
+++ hello.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello world!</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
@@ -14,4 +16,4 @@ def user(name):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
