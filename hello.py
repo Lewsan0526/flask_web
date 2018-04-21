83d0823899a7d982c7805e1c11cafc9e74a5a309
lewsan <liujintong@peiwo.cn>
2018-04-21 22:58:42 +0800

    feat:add session_name update_check

--- hello.py
+++ hello.py
@@ -1,10 +1,9 @@
-from datetime import datetime
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "hard to guess string"
@@ -14,6 +13,7 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('email address:', validators=[Email()])
     submit = SubmitField('Submit')
 
 
@@ -31,8 +31,10 @@ def interval_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
-        return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'))
 
 
